7d68860f39cd75e15bf5bd1da37ee126b7e2a174
Bhaktivetal1202 <142563505+Bhaktivetal1202@users.noreply.github.com>
2024-03-03 18:52:57 +0530

    Add files via upload

---
+++ loop16.cpp
@@ -0,0 +1,13 @@
+/* c program to display n natural numbers */
+#include<stdio.h>
+void  main()
+{
+	int i,n;
+	printf("enter the limit=");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\t",i);
+	}
+}
+
